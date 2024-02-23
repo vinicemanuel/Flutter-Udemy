d6bdab2e1b0545ebeaac94828dd37507daee60b1
vinicius emanuel <vinicius.emanuel@thoughtworks.com>
2024-02-23 10:49:51 -0300

    s4 - 84

---
+++ expenses/lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'Pages/home_page.dart';
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
