f288dd2a66338edfe53d31c0ab447829ac726bb7
vinicius emanuel <vinicius.emanuel@thoughtworks.com>
2024-02-23 10:59:47 -0300

    s4 - 85

--- expenses/lib/Pages/home_page.dart
+++ expenses/lib/Pages/home_page.dart
@@ -6,10 +6,23 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Despesas Pessoais"),
-      ),
-      body: Center(child: Text("Home")),
-    );
+        appBar: AppBar(
+          title: const Text("Despesas Pessoais"),
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 5,
+                color: Colors.blue,
+                child: Text("grafico"),
+              ),
+            ),
+            Card(
+              child: Text("Lista"),
+            )
+          ],
+        ));
   }
 }
